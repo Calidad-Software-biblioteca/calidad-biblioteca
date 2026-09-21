c283fae107d9e2f9f5ed4fd6d87c831fc14ff73b
irolon <i.rolonmarecos@gmail.com>
2026-09-21 20:46:12 -0300

    Revert "fix: corregir método calcularMulta"
    This reverts commit 5eb303a5996729e229114854f54006c4e7ff78ea.

--- src/main/java/com/biblioteca/service/PrestamoService.java
+++ src/main/java/com/biblioteca/service/PrestamoService.java
@@ -140,51 +140,52 @@ public class PrestamoService {
     }
 
 
-   public double calcularMulta(Prestamo prestamo) {
+    public double calcularMulta(Prestamo prestamo) {
 
-    if (prestamo == null) {
-        return 0;
-    }
+        if (prestamo == null) {
+            return 0;
+        }
 
-    if (!prestamo.isDevuelto()) {
+        if (!prestamo.isDevuelto()) {
+
+            long dias = ChronoUnit.DAYS.between(
+                    prestamo.getFechaPrestamo(),
+                    LocalDate.now());
+
+            if (dias <= 7) {
+                return 0;
+            }
+
+            if (dias <= 14) {
+                return (dias - 7) * 100;
+            }
+
+            if (dias <= 30) {
+                return (dias - 7) * 200;
+            }
+
+            return (dias - 7) * 500;
+        }
 
         long dias = ChronoUnit.DAYS.between(
                 prestamo.getFechaPrestamo(),
-                LocalDate.now());
+                prestamo.getFechaDevolucion());
 
+        // DUPLICACIÓN INTENCIONAL
         if (dias <= 7) {
             return 0;
         }
 
         if (dias <= 14) {
-            return (dias - 7) * 1000;
+            return (dias - 7) * 100;
         }
 
         if (dias <= 30) {
-            return (dias - 7) * 2000;
+            return (dias - 7) * 200;
         }
 
-        return (dias - 7) * 5000;
+        return (dias - 7) * 500;
     }
-
-    long dias = ChronoUnit.DAYS.between(
-            prestamo.getFechaPrestamo(),
-            prestamo.getFechaDevolucion());
-
-    if (dias <= 7) {
-        return 0;
-    }
-
-    if (dias <= 14) {
-        return (dias - 7) * 1000;
-    }
-
-    if (dias <= 30) {
-        return (dias - 7) * 2000;
-    }
-
-    return (dias - 7) * 5000;
-}
 
     public List<Prestamo> listarPrestamos() {
         return prestamoRepository.findAll();
